7e24fe2800264ed694f5a775047d1c013336ab8b
mike-freislich <mike.freislich@gmail.com>
2022-03-06 17:07:54 +0200

    turret AI

--- Assets/Base/Enemy.cs
+++ Assets/Base/Enemy.cs
@@ -7,9 +7,23 @@ public class Enemy : MonoBehaviour, IDamageable
     public int health;
 
     public GameObject explosion;
+    private GameObject playerObject;
 
     public int currentHealth { get { return health; } }
 
+    private void Awake()
+    {
+        if (playerObject == null)
+            playerObject = GameObject.FindWithTag("Player");
+    }
+
+    public GameObject player
+    {
+        get
+        {
+            return playerObject;
+        }
+    }
 
     public void takeDamage(int damage)
     {
@@ -23,7 +37,7 @@ public class Enemy : MonoBehaviour, IDamageable
     protected virtual void explode()
     {
         Vector3 pos = transform.position;
-        Vector3 extents = explosion.GetComponent<SpriteRenderer>().bounds.extents;
+        Vector3 extents = explosion.GetComponent<SpriteRenderer>().bounds.extents;        
         pos += new Vector3(0, extents.y - 0.5f, 0);
         Instantiate(explosion, pos, new Quaternion());
         Destroy(gameObject);
--- Assets/Prefabs/Turret/Turret.cs
+++ Assets/Prefabs/Turret/Turret.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 
 public class Turret : Enemy
 {
-    
+    float heightThreshold;
+
     void Start()
-    {        
-        
+    {
+        heightThreshold = -0.5f;
     }
 
     private void FixedUpdate()
-    {       
+    {
+        aimCanon();
     }
 
+    private void aimCanon()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 myPos = transform.position;
+        Vector3 posDelta = playerPos - myPos;
+
+        Animator anim = GetComponent<Animator>();
+        int layerIndex = anim.GetLayerIndex("Base Layer");
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(layerIndex);        
+
+        // LOW
+        if (stateInfo.IsName("Idle Left Low"))
+        {            
+            if (posDelta.x > 0) anim.Play("rotate Right Low");
+            else if (posDelta.y + heightThreshold > 0) anim.Play("up Left");                
+        }
+
+        else if (stateInfo.IsName("Idle Right Low"))
+        {
+            if (posDelta.x < 0) anim.Play("rotate Left Low");
+            else if (posDelta.y + heightThreshold > 0) anim.Play("up Right");    
+        }
+
+        // HIGH 
+        else if (stateInfo.IsName("Idle Left High"))
+        {
+            if (posDelta.x > 0) anim.Play("rotate Right High");
+            else if (posDelta.y + heightThreshold < 0) anim.Play("down Left");            
+        }
+        
+        
+        else if (stateInfo.IsName("Idle Right High"))
+        {
+            if (posDelta.x < 0) anim.Play("rotate Left High");
+            else if (posDelta.y + heightThreshold < 0) anim.Play("down Right");
+        }
+
+
+
+    }
 }
